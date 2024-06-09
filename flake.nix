9b82bfb9a88558b6dcf9f7f4d20a25a90af6c89d
Daksh Gupta <acedaksh07@gmail.com>
2024-06-09 17:12:01 -0400

    build: nix
    Export a nix package.

--- flake.nix
+++ flake.nix
@@ -1,37 +1,59 @@
 {
-  description = "Pibar ~ A Clock Thing";
+  description = "chill-mp ~ A chill music player.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in {
-        devShells.default = pkgs.mkShell rec {
+
+        craneLib = crane.lib.${system};
+        gen-args = {
+          buildInputs = [
+            # Add additional build inputs here
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+
+        };
+        cli-crate = craneLib.buildPackage (gen-args // {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+        });
+      in
+      {
+        checks = {};
+
+        packages.default = cli-crate;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = cli-crate;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks.${system};
+
           # Additional dev-shell environment variables can be set directly
           # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
 
-
+          # Extra inputs can be added here
           nativeBuildInputs = with pkgs; [
             cargo
             rustc
             rust-analyzer
-            cargo-watch
           ];
-          buildInputs = with pkgs; [
-            wayland
-            wayland.dev
-            libGL
-            libxkbcommon
-            pkg-config
-          ];
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
         };
       });
 }
